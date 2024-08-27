b9607c49839ca6ed97df586b411f5f42762a32d7
Sunny Kumar <sunnykumar49617@gmail.com>
2024-08-28 01:07:49 +0530

    all set up configured

--- src/index.js
+++ src/index.js
@@ -1,42 +1,51 @@
 // require('dotenv').config({path: './env'})
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
-
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-	path: './.env'
+    path: './.env'
 })
+
+
 
 connectDB()
 .then(() => {
-	app.listen(process.env.PORT || 8000, () => {
-		console.log('listening on port ' + process.env.PORT)
-	})
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
 })
 .catch((err) => {
-	console.error(`Error connecting to MongoDB: ${err.message}`);
-    process.exit(1);
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
+
+
+
+
+
+
 
 
 /*
-import express from 'express'
+import express from "express"
 const app = express()
 ( async () => {
-	try {
-		await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-		app.on('error', (error) => {
-			console.error("MongoDB connection error:", error);
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
             throw error
-		})
-		
-		app.listen(process.env.PORT, () => {
-			console.log(`Server is running on port ${process.env.PORT}`);
-		})
-	} catch (error) {
-		console.error("ERROR", error);
-		throw err
-	}
-})()
-	
-*/
+        })
 
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
--- src/utils/ApiResponce.js
+++ src/utils/ApiResponce.js
@@ -1,8 +1,8 @@
 class ApiResponse {
-	constructor(statusCode, message = "Success", data) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
-		this.success = statusCode < 400
-    }
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
 }
