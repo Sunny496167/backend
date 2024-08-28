461d7d47d2a9730e3046a63b240295a3d3f870b8
Sunny Kumar <sunnykumar49617@gmail.com>
2024-08-29 04:04:54 +0530

    register controller bug fix

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,7 +56,7 @@ UserSchema.methods.isPasswordCorrect = async function(password) {
 	return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateAccessToken = function () {
+UserSchema.methods.generateAccessToken = function () {
 	return jwt.sign(
 		{
 			_id: this._id,
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken = function () {
 	)
 }
 
-userSchema.methods.generateRefreshToken = function () {
+UserSchema.methods.generateRefreshToken = function () {
 	return jwt.sign(
 		{
 			_id: this._id,
